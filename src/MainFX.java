0aa5f035077a55e9fdf7abac870b3db12bfd615e
Bruno Tardin <brunotardin20@gmail.com>
2019-04-16 12:08:36 -0300

    Lógica da seleção dos filtros de mídia implementada

--- src/MainFX.java
+++ src/MainFX.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -13,7 +7,7 @@ import javafx.stage.Stage;
 
 /**
  *
- * @author btardin
+ * @author 8rux40
  */
 public class MainFX extends Application {
     private static Scene scene;
--- src/controller/TestefxController.java
+++ src/controller/TestefxController.java
@@ -2,21 +2,148 @@ package controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
  *
- * @author btardin
+ * @author 8rux40
  */
 public class TestefxController implements Initializable {
-
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private BorderPane borderPaneMain;
+    @FXML
+    private Pane topPane;
+    @FXML
+    private RadioButton rbTodas;
+    @FXML
+    private ToggleGroup midia;
+    @FXML
+    private RadioButton rbOutra;
+    @FXML
+    private CheckBox cbCd;
+    @FXML
+    private Button btnNovoAlbum;
+    @FXML
+    private CheckBox cbDvd;
+    @FXML
+    private CheckBox cbVinil;
+    @FXML
+    private CheckBox cbBluray;
+    @FXML
+    private CheckBox cbK7;
+    @FXML
+    private Pane leftPane;
+    @FXML
+    private ScrollPane scrollAlbuns;
+    @FXML
+    private AnchorPane anchorAlbuns;
+    @FXML
+    private Pane rightPane;
+    @FXML
+    private Label filtroTodas;
+    @FXML
+    private Label filtroCd;
+    @FXML
+    private Label filtroDvd;
+    @FXML
+    private Label filtroVinil;
+    @FXML
+    private Label filtroBluray;
+    @FXML
+    private Label filtroK7;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+    
+    private void removerAlbum(){
+        
+    }
+    
+    private void editarAlbum(){
+        
+    }
+    
+    private void novoAlbum(){
+        
+    }
+    
+    private void ativaDesativaFiltro(CheckBox cbFiltro, Label filtroLbl){
+        boolean status = cbFiltro.isSelected();
+        if (!status) {
+            rbOutra.setSelected(true);
+            ativaDesativaFiltro(false, filtroTodas);
+        }
+        cbFiltro.setSelected(!status);
+        ativaDesativaFiltro(!status, filtroLbl);
+    }
+    
+    private void ativaDesativaFiltro(boolean status, Label filtroLbl){
+        if(!status){
+            filtroLbl.getStyleClass().remove("etiqueta-grande-selecionada");
+            filtroLbl.getStyleClass().add("etiqueta-grande");
+        } else {
+            filtroLbl.getStyleClass().remove("etiqueta-grande");
+            filtroLbl.getStyleClass().add("etiqueta-grande-selecionada");
+        }
+    }  
+    
+    private void desativaFiltros(){
+        cbCd.setSelected(false);
+        cbDvd.setSelected(false);
+        cbVinil.setSelected(false);
+        cbBluray.setSelected(false);
+        cbK7.setSelected(false);
+        ativaDesativaFiltro(false, filtroCd);
+        ativaDesativaFiltro(false, filtroDvd);
+        ativaDesativaFiltro(false, filtroVinil);
+        ativaDesativaFiltro(false, filtroBluray);
+        ativaDesativaFiltro(false, filtroK7);
+    }
+
+    @FXML
+    private void onFiltroTodasClicked(MouseEvent event) {
+        rbTodas.setSelected(true);
+        ativaDesativaFiltro(true, filtroTodas);
+        desativaFiltros();
+    }
+
+    @FXML
+    private void onFiltroCdClicked(MouseEvent event) {
+        ativaDesativaFiltro(cbCd, filtroCd);
+    }
+
+    @FXML
+    private void onFiltroDvdClicked(MouseEvent event) {
+        ativaDesativaFiltro(cbDvd, filtroDvd);
+    }
+
+    @FXML
+    private void onFiltroVinilClicked(MouseEvent event) {
+        ativaDesativaFiltro(cbVinil, filtroVinil);
+    }
+
+    @FXML
+    private void onFiltroBlurayClicked(MouseEvent event) {
+        ativaDesativaFiltro(cbBluray, filtroBluray);
+    }
+
+    @FXML
+    private void onFiltroK7Clicked(MouseEvent event) {
+        ativaDesativaFiltro(cbK7, filtroK7);
+    }
     
 }
