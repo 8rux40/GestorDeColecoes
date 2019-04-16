48f76431502868f30ce277ddc0ffa7e208b854b8
Bruno Tardin <brunotardin20@gmail.com>
2019-04-16 10:02:08 -0300

    Tela inicial do Sistema formulada

--- src/MainFX.java
+++ src/MainFX.java
@@ -4,12 +4,11 @@
  * and open the template in the editor.
  */
 
+import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
@@ -17,34 +16,28 @@ import javafx.stage.Stage;
  * @author btardin
  */
 public class MainFX extends Application {
+    private static Scene scene;
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        try {
+            Parent parent = (Parent) FXMLLoader.load(getClass().getResource("/view/testefx.fxml"));
+            scene = new Scene(parent);
+            primaryStage.setScene(scene);
+            primaryStage.setMaximized(true);
+//            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
+    }
+    
+    public static Scene getScene(){
+        return MainFX.scene;
     }
     
 }
